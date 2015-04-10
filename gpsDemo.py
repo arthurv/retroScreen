47b86f191f1ec0b288ec36f19b7dad18ea715994
arthur <aonsquared@gmail.com>
2015-04-10 21:32:27 +0100

    Basic GPS operation working. TODO: fix handling.

--- gpsDemo.py
+++ gpsDemo.py
@@ -1,7 +1,19 @@
 #! /usr/bin/python
 # Written by Dan Mandle http://dan.mandle.me September 2012
 # License: GPL 2.0
- 
+
+import io
+import psutil
+import signal
+import sys
+
+import Adafruit_Nokia_LCD as LCD
+import Adafruit_GPIO.SPI as SPI
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
 import os
 from gps import *
 from time import *
@@ -10,7 +22,44 @@ import threading
  
 gpsd = None #seting the global variable
  
-os.system('clear') #clear the terminal (optional)
+# Raspberry Pi hardware SPI config:
+DC = 23
+RST = 24
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+# Hardware SPI usage:
+disp = LCD.PCD8544(DC, RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=4000000))
+
+# Software SPI usage (defaults to bit-bang SPI interface):
+#disp = LCD.PCD8544(DC, RST, SCLK, DIN, CS)
+
+# Initialize library.
+disp.begin(contrast=35)
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+image = Image.new('1', (LCD.LCDWIDTH, LCD.LCDHEIGHT))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Load default font.
+font = ImageFont.load_default()
+
+def signal_term_handler(signum = None, frame = None):
+  sys.stderr.write("Terminated.\n")
+  draw.rectangle((0,0,83,47), outline=255, fill=255)
+  disp.image(image)
+  disp.display()
+  sys.exit(0)
+
+for sig in [signal.SIGTERM, signal.SIGINT, signal.SIGHUP, signal.SIGQUIT]: 
+  signal.signal(sig, signal_term_handler)
  
 class GpsPoller(threading.Thread):
   def __init__(self):
@@ -33,26 +82,19 @@ if __name__ == '__main__':
       #It may take a second or two to get good data
       #print gpsd.fix.latitude,', ',gpsd.fix.longitude,'  Time: ',gpsd.utc
  
-      os.system('clear')
- 
-      print
-      print ' GPS reading'
-      print '----------------------------------------'
-      print 'latitude    ' , gpsd.fix.latitude
-      print 'longitude   ' , gpsd.fix.longitude
-      print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
-      print 'altitude (m)' , gpsd.fix.altitude
-      print 'eps         ' , gpsd.fix.eps
-      print 'epx         ' , gpsd.fix.epx
-      print 'epv         ' , gpsd.fix.epv
-      print 'ept         ' , gpsd.fix.ept
-      print 'speed (m/s) ' , gpsd.fix.speed
-      print 'climb       ' , gpsd.fix.climb
-      print 'track       ' , gpsd.fix.track
-      print 'mode        ' , gpsd.fix.mode
-      print
-      print 'sats        ' , gpsd.satellites
- 
+      draw.rectangle((0,0,83,47), outline=255, fill=255)
+      timestr = time.strftime("%H:%M:%S", time.localtime())
+      draw.text((0,0), timestr, font=font)
+      latstr = 'lat:' + str(gpsd.fix.latitude)
+      draw.text((0,10), latstr, font=font)
+      longstr = 'lon:' + str(gpsd.fix.longitude)
+      draw.text((0,20), longstr, font=font)
+      #print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
+      #print 'altitude (m)' , gpsd.fix.altitude
+      #print 'speed (m/s) ' , gpsd.fix.speed
+      #print 'sats        ' , gpsd.satellites
+      disp.image(image)
+      disp.display()
       time.sleep(5) #set to whatever
  
   except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
