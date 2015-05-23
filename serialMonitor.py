4bd6cfadfe7c3c52628c0da6e124dc5dfb927377
arthur <aonsquared@gmail.com>
2015-05-23 09:05:06 +0100

    add serial Monitor script

---
+++ serialMonitor.py
@@ -0,0 +1,76 @@
+import io
+import time
+import psutil
+import signal
+import sys
+
+import Adafruit_Nokia_LCD as LCD
+import Adafruit_GPIO.SPI as SPI
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+# Raspberry Pi hardware SPI config:
+DC = 23
+RST = 24
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+# Hardware SPI usage:
+disp = LCD.PCD8544(DC, RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=4000000))
+
+# Initialize library.
+disp.begin(contrast=85)
+disp.set_contrast(55)
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+image = Image.new('1', (LCD.LCDWIDTH, LCD.LCDHEIGHT))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Load default font.
+font = ImageFont.load_default()
+
+def signal_term_handler(signum = None, frame = None):
+	sys.stderr.write("Terminated.\n")
+	tFile.close()
+	disp.clear()
+	disp.display()
+	sys.exit(0)
+
+for sig in [signal.SIGTERM, signal.SIGINT, signal.SIGHUP, signal.SIGQUIT]: 
+	signal.signal(sig, signal_term_handler)
+
+#sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+try:
+	while 1:
+		# Clear image buffer.
+		draw.rectangle((0,0,83,47), outline=255, fill=255)
+		#disp.clear()
+		cpustr = "CPU: " + str(psutil.cpu_percent(interval=None)) + "%"
+		draw.text((0,00), cpustr, font=font)
+		memstr = "Mem: " + str(psutil.virtual_memory()[2]) + "%"
+		draw.text((0,10), memstr, font=font)
+		diskstr = "Disk: " + str(psutil.disk_usage("/")[3]) + "%"
+		draw.text((0,20), diskstr, font=font)
+		#clock
+		#timestr = time.strftime("%H:%M:%S", time.localtime())
+		tFile = open('/sys/class/thermal/thermal_zone0/temp')
+		temp = "Temp: " + "{:.2f}".format(float(tFile.read())/1000) + " C"
+		draw.text((0,30), temp, font=font)
+		# Display image.
+		disp.image(image)
+		disp.display()
+        	time.sleep(1)
+except:
+	tFile.close()
+	# clear display.
+	disp.clear()
+	disp.display()
